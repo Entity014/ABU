811c846cff04d6bef1446b84dcdad1ed43f79006
_Entity014 ( Narawit Inyos ) <63171588+Entity014@users.noreply.github.com>
2023-04-24 00:10:52 +0700

    Update detectColor.py

--- swap_pillar/swap_pillar/detectColor.py
+++ swap_pillar/swap_pillar/detectColor.py
@@ -33,7 +33,7 @@ class ImagePublisher(Node):
     # The argument '0' gets the default webcam.
     # self.GSTREAMER_PIPELINE = 'nvarguscamerasrc ! video/x-raw(memory:NVMM), width=720, height=490, format=(string)NV12, framerate=30/1 ! nvvidconv flip-method=0 ! video/x-raw, width=720, height=480, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink'
     # self.cap = cv2.VideoCapture(self.GSTREAMER_PIPELINE, cv2.CAP_GSTREAMER)
-
+    
     self.cap = cv2.VideoCapture(0)
 
     # Used to convert between ROS and OpenCV images
@@ -96,7 +96,8 @@ class ImagePublisher(Node):
       # Publish the image.
       # The 'cv2_to_imgmsg' method converts an OpenCV
       # image to a ROS 2 image message
-      self.publisher_.publish(self.br.cv2_to_imgmsg(frame))
+      
+      self.publisher_.publish(self.br.cv2_to_imgmsg(frame, 'bgr8'))
       self.publisher2_.publish(msg)
       if cv2.waitKey(1) & 0xFF == ord('q'):
         #closing all open windows 
