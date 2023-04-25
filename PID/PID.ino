5e26957e21f176502a007a6be1343134725ed266
_Entity014 ( Narawit Inyos ) <63171588+Entity014@users.noreply.github.com>
2023-04-26 01:03:47 +0700

    Update PID.ino

--- PID/PID.ino
+++ PID/PID.ino
@@ -1,22 +1,40 @@
 #include <PID_v1.h>
 #include <analogWrite.h>
+#include <SimpleKalmanFilter.h>
 
-int LPWM_Output1 = 14;
-int RPWM_Output1 = 12;
+int LPWM_Output1 = 12;
+int RPWM_Output1 = 14;
 uint8_t encoder1 = 25;
 
 int LPWM_Output2 = 26;
 int RPWM_Output2 = 27;
 uint8_t encoder2 = 33;
 
-volatile unsigned int counter1, counter2;
+int counter1, counter2;
 double Setpoint, Input1, Input2, Output1, Output2;
+double Infil1, Infil2;
 
-double Kp=0.15, Ki=0.2, Kd=0.0125;
- 
-void setup()
-{
-  Setpoint = 400;
+// double Kp = 5e-4, Ki = 0.02, Kd = 1.0;
+double Kp = 5e-4, Ki = 0.035, Kd = 1.0;
+
+SimpleKalmanFilter simpleKalmanFilter(2, 2, 0.001);
+
+void countpulse1() {
+  counter1++;
+}
+
+void countpulse2() {
+  counter2++;
+}
+
+double roundPerMin(int &counter) {
+  double round;
+  round = (counter / 40) * 600;
+  counter = 0;
+  return round;
+}
+
+void setup() {
 
   Serial.begin(9600);
   pinMode(encoder1, INPUT);
@@ -31,52 +49,23 @@ void setup()
 
   attachInterrupt(encoder1, countpulse1, RISING);
   attachInterrupt(encoder2, countpulse2, RISING);
+
+  Setpoint = 5000;
 }
 
-void countpulse1()
-{
-  counter1++;
-}
+void loop() {
+  static uint32_t previousMillis1;
 
-void countpulse2()
-{
-  counter2++;
-}
- 
-double preRound = 0;
-double roundPerMin(volatile unsigned int &counter)
-{
-  static uint32_t previousMillis;
-  double round;
-  if (millis() - previousMillis >= 1000 )
-  {
-    round = (counter/40) * 60;
-    counter = 0;
-    previousMillis += 1000;
-    if (round <= 900)
-    {
-      preRound = round;
-      return round;
-    }
-    else return preRound;
-  }
-}
-
-double inputB = 0;
-
-void loop()
-{
-  if (Serial.available() > 0)
-  {
-    inputB = (double(char(Serial.read()) - '0') * 0.1);
-    Kd = inputB;
+  while (millis() - previousMillis1 >= 100) {
+    Input1 = roundPerMin(counter1);
+    Input2 = roundPerMin(counter2);
+    Infil1 = simpleKalmanFilter.updateEstimate(Input1);
+    Infil2 = simpleKalmanFilter.updateEstimate(Input2);
+    previousMillis1 += 100;
   }
 
-  Input1 = roundPerMin(counter1);
-  Input2 = roundPerMin(counter2);
-
-  PID myPID1(&Input1, &Output1, &Setpoint, Kp, Ki, Kd, DIRECT);
-  PID myPID2(&Input2, &Output2, &Setpoint, Kp, Ki, Kd, DIRECT);
+  PID myPID1(&Infil1, &Output1, &Setpoint, Kp, Ki, Kd, DIRECT);
+  PID myPID2(&Infil2, &Output2, &Setpoint, Kp, Ki, Kd, DIRECT);
   myPID1.SetMode(AUTOMATIC);
   myPID2.SetMode(AUTOMATIC);
   myPID1.Compute();
@@ -84,9 +73,9 @@ void loop()
   // Serial.println(myPID.GetKp());
 
   analogWrite(LPWM_Output1, Output1);
+  // analogWrite(LPWM_Output1, 255);
   analogWrite(RPWM_Output1, 0);
   analogWrite(LPWM_Output2, 0);
   analogWrite(RPWM_Output2, Output2);
-  Serial.println(String(Setpoint)+" "+String(Input1)+" "+String(Output1) + " " + String(abs(Input1 - Setpoint)) + " " + String(myPID1.GetKd()));
-  delay(100);
+  Serial.println(String(Setpoint) + " " + String(Infil1) + " " + String(Infil2) + " " + String(Output1) + " " + String(abs(Infil1 - Setpoint)) + " " + String(myPID1.GetKp()) + " " + String(myPID1.GetKi()) + " " + String(myPID1.GetKd()));
 }
