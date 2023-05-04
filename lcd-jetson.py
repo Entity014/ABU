35d16a8ff8a648caecf848f3a33d9fdcde307be2
_Entity014 <phytes.narawit@gmail.com>
2023-05-04 07:18:49 +0700

    v.12

--- lcd-jetson.py
+++ lcd-jetson.py
@@ -17,110 +17,63 @@ lcd_rows = 2
 
 # Initialize LCD
 lcd = characterlcd.Character_LCD_Mono(
-    lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows)
+    lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows
+)
 
-leftSide = bytes([
-    0B00111,
-    0B01111,
-    0B01111,
-    0B01111,
-    0B01111,
-    0B01111,
-    0B01111,
-    0B00111])
+leftSide = bytes(
+    [0b00111, 0b01111, 0b01111, 0b01111, 0b01111, 0b01111, 0b01111, 0b00111]
+)
 
-upperBar = bytes([
-    0B11111,
-    0B11111,
-    0B11111,
-    0B00000,
-    0B00000,
-    0B00000,
-    0B00000,
-    0B00000])
+upperBar = bytes(
+    [0b11111, 0b11111, 0b11111, 0b00000, 0b00000, 0b00000, 0b00000, 0b00000]
+)
 
-rightSide = bytes([
-    0B11100,
-    0B11110,
-    0B11110,
-    0B11110,
-    0B11110,
-    0B11110,
-    0B11110,
-    0B11100])
+rightSide = bytes(
+    [0b11100, 0b11110, 0b11110, 0b11110, 0b11110, 0b11110, 0b11110, 0b11100]
+)
 
-leftEnd = bytes([
-    0B01111,
-    0B00111,
-    0B00000,
-    0B00000,
-    0B00000,
-    0B00000,
-    0B00011,
-    0B00111
-])
+leftEnd = bytes(
+    [0b01111, 0b00111, 0b00000, 0b00000, 0b00000, 0b00000, 0b00011, 0b00111]
+)
 
-lowerBar = bytes([
-    0B00000,
-    0B00000,
-    0B00000,
-    0B00000,
-    0B00000,
-    0B11111,
-    0B11111,
-    0B11111
-])
+lowerBar = bytes(
+    [0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b11111, 0b11111, 0b11111]
+)
 
-rightEnd = bytes([
-    0B11110,
-    0B11100,
-    0B00000,
-    0B00000,
-    0B00000,
-    0B00000,
-    0B11000,
-    0B11100
-])
+rightEnd = bytes(
+    [0b11110, 0b11100, 0b00000, 0b00000, 0b00000, 0b00000, 0b11000, 0b11100]
+)
 
-middleBar = bytes([
-    0B11111,
-    0B11111,
-    0B11111,
-    0B00000,
-    0B00000,
-    0B00000,
-    0B11111,
-    0B11111
-])
+middleBar = bytes(
+    [0b11111, 0b11111, 0b11111, 0b00000, 0b00000, 0b00000, 0b11111, 0b11111]
+)
 
-lowerEnd = bytes([
-    0B00000,
-    0B00000,
-    0B00000,
-    0B00000,
-    0B00000,
-    0B00000,
-    0B00111,
-    0B01111
-])
+lowerEnd = bytes(
+    [0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b00111, 0b01111]
+)
 
-obj_num = [leftSide, upperBar, rightSide, leftEnd,
-           lowerBar, rightEnd, middleBar, lowerEnd]
+obj_num = [
+    leftSide,
+    upperBar,
+    rightSide,
+    leftEnd,
+    lowerBar,
+    rightEnd,
+    middleBar,
+    lowerEnd,
+]
 
 for i in range(len(obj_num)):
     lcd.create_char(i, obj_num[i])
 
 
 def displayInt(n: int, x: bytes, y: bytes, digits: bytes, leading: bool):
-    isNegative = False
-
-    if (n < 0):
-        isNegative = True
+    if n < 0:
         n = abs(n)
     numString = []
     ind = digits - 1
 
-    while (ind):
+    while ind:
         numString[ind] = n % 10
         n /= 10
         ind -= 1
@@ -146,52 +99,52 @@ def displayNumber(n: bytes, x: bytes, y: bytes):
     if n == 0:
         lcd.cursor_position(x, y)
         lcd.message("\x00\x01\x02")
-        lcd.cursor_position(x, y+1)
+        lcd.cursor_position(x, y + 1)
         lcd.message("\x00\x04\x02")
     elif n == 1:
         lcd.cursor_position(x, y)
         lcd.message("  \x02")
-        lcd.cursor_position(x, y+1)
+        lcd.cursor_position(x, y + 1)
         lcd.message("  \x02")
     elif n == 2:
         lcd.cursor_position(x, y)
         lcd.message("\x03\x06\x02")
-        lcd.cursor_position(x, y+1)
+        lcd.cursor_position(x, y + 1)
         lcd.message("\x00\x04\x04")
     elif n == 3:
         lcd.cursor_position(x, y)
         lcd.message("\x03\x06\x02")
-        lcd.cursor_position(x, y+1)
+        lcd.cursor_position(x, y + 1)
         lcd.message("\x07\x04\x02")
     elif n == 4:
         lcd.cursor_position(x, y)
         lcd.message("\x00\x04\x02")
-        lcd.cursor_position(x, y+1)
+        lcd.cursor_position(x, y + 1)
         lcd.message("  \x02")
     elif n == 5:
         lcd.cursor_position(x, y)
         lcd.message("\x00\x06\x02")
-        lcd.cursor_position(x, y+1)
+        lcd.cursor_position(x, y + 1)
         lcd.message("\x07\x04\x02")
     elif n == 6:
         lcd.cursor_position(x, y)
         lcd.message("\x00\x06\x05")
-        lcd.cursor_position(x, y+1)
+        lcd.cursor_position(x, y + 1)
         lcd.message("\x00\x04\x02")
     elif n == 7:
         lcd.cursor_position(x, y)
         lcd.message("\x01\x01\x02")
-        lcd.cursor_position(x, y+1)
+        lcd.cursor_position(x, y + 1)
         lcd.message("  \x02")
     elif n == 8:
         lcd.cursor_position(x, y)
         lcd.message("\x00\x06\x02")
-        lcd.cursor_position(x, y+1)
+        lcd.cursor_position(x, y + 1)
         lcd.message("\x00\x04\x02")
     elif n == 9:
         lcd.cursor_position(x, y)
         lcd.message("\x00\x06\x02")
-        lcd.cursor_position(x, y+1)
+        lcd.cursor_position(x, y + 1)
         lcd.message("\x07\x04\x02")
 
 
