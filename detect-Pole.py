373896040b139649f9effd39379d4cca9f9c08ce
_Entity014 <phytes.narawit@gmail.com>
2023-05-11 12:07:36 +0700

    OpenCV upgrade

--- detect-Pole.py
+++ detect-Pole.py
@@ -6,7 +6,7 @@ cap = cv2.VideoCapture(0)
 
 width = float(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = float(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-vector_cen = np.array([width/2, height/2])
+vector_cen = np.array([width / 2, height / 2])
 
 deltha_dis_min = []
 deltha_dis = []
@@ -16,10 +16,10 @@ near_point = 0
 abs_near_point = 0
 
 pre_num = 0
-# lower_blue = np.array([95, 89, 75])
-# upper_blue = np.array([107, 255, 255])
-lower_blue = np.array([19, 89, 88])
-upper_blue = np.array([28, 255, 255])
+lower_blue = np.array([84, 90, 127])
+upper_blue = np.array([105, 255, 255])
+# lower_blue = np.array([19, 89, 88])
+# upper_blue = np.array([28, 255, 255])
 
 # focal_length = 360
 # object_width = 100
@@ -43,24 +43,23 @@ while True:
     # res = cv2.bitwise_and(frame, frame, mask=mask)
 
     # Find contours
-    contours, hierarchy = cv2.findContours(
-        mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     center_coordinates = (int(vector_cen[0]), int(vector_cen[1]))
 
     # Loop through contours and detect shapes
     for cnt in contours:
-        approx = cv2.approxPolyDP(cnt, 0.01*cv2.arcLength(cnt, True), True)
+        approx = cv2.approxPolyDP(cnt, 0.01 * cv2.arcLength(cnt, True), True)
         area = cv2.contourArea(cnt)
 
-        if area > 500:
+        if area > 500 and len(approx) >= 4 and len(approx) <= 10:
             x, y, w, h = cv2.boundingRect(cnt)
             # if len(approx) > 4:
             if h / w > 0.5:
                 num += 1
-                center_obj = (int(x + (w/2)), int(height/2))
+                center_obj = (int(x + (w / 2)), int(height / 2))
                 # distance_point = sqrt(pow(vector_cen[0] - (x + (w/2)), 2))
-                distance_point = vector_cen[0] - (x + (w/2))
+                distance_point = vector_cen[0] - (x + (w / 2))
                 deltha_dis_min.append(abs(distance_point))
                 deltha_dis.append(distance_point)
 
@@ -73,8 +72,7 @@ while True:
                 elif num == 1:
                     pre_num = num
                     abs_near_point = min(deltha_dis_min)
-                    near_point = deltha_dis[deltha_dis_min.index(
-                        abs_near_point)]
+                    near_point = deltha_dis[deltha_dis_min.index(abs_near_point)]
                     # near_dis = round(
                     #     distance_Cam[deltha_dis.index(near_point)], 2)
                     deltha_dis.clear()
@@ -83,19 +81,26 @@ while True:
 
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
                 cv2.drawContours(frame, [cnt], 0, (255, 255, 0), -1)
-                cv2.putText(frame, f'C{num} [ {(distance_point):.2f}]',
-                            (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+                cv2.putText(
+                    frame,
+                    f"C{num} [ {(len(approx)):.2f}]",
+                    (x, y),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,
+                    (255, 0, 0),
+                    2,
+                )
                 cv2.circle(frame, center_obj, 5, (0, 0, 0), -1)
                 cv2.line(frame, center_coordinates, center_obj, (255, 0, 0), 2)
 
-    print(near_point, abs_near_point)
+    # print(near_point, abs_near_point)
     cv2.circle(frame, center_coordinates, 5, (0, 0, 255), -1)
     # Display the resulting frame
-    cv2.imshow('frame', frame)
-    cv2.imshow('mask', mask)
+    cv2.imshow("frame", frame)
+    cv2.imshow("mask", mask)
 
     # Quit program when 'q' key is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
     if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
         cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
