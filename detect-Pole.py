23c5ca8b704595b829a64adef26022af883238d2
_Entity014 <phytes.narawit@gmail.com>
2023-05-11 12:52:09 +0700

    shape Detect blue

--- detect-Pole.py
+++ detect-Pole.py
@@ -16,10 +16,10 @@ near_point = 0
 abs_near_point = 0
 
 pre_num = 0
-lower_blue = np.array([84, 90, 127])
-upper_blue = np.array([105, 255, 255])
-# lower_blue = np.array([19, 89, 88])
-# upper_blue = np.array([28, 255, 255])
+lower_range = np.array([84, 90, 127])
+upper_range = np.array([105, 255, 255])
+# lower_range = np.array([19, 89, 88])
+# upper_range = np.array([28, 255, 255])
 
 # focal_length = 360
 # object_width = 100
@@ -33,7 +33,7 @@ while True:
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     # Threshold the HSV image to get only blue colors
-    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    mask = cv2.inRange(hsv, lower_range, upper_range)
 
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
     mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
@@ -51,8 +51,7 @@ while True:
     for cnt in contours:
         approx = cv2.approxPolyDP(cnt, 0.01 * cv2.arcLength(cnt, True), True)
         area = cv2.contourArea(cnt)
-
-        if area > 500 and len(approx) >= 4 and len(approx) <= 10:
+        if area > 500 and len(approx) >= 4 and len(approx) <= 10:  # ? Blue
             x, y, w, h = cv2.boundingRect(cnt)
             # if len(approx) > 4:
             if h / w > 0.5:
@@ -79,21 +78,21 @@ while True:
                     deltha_dis_min.clear()
                     # distance_Cam.clear()
 
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 0), 2)
                 cv2.drawContours(frame, [cnt], 0, (255, 255, 0), -1)
                 cv2.putText(
                     frame,
-                    f"C{num} [ {(len(approx)):.2f}]",
+                    f"Pole<{num}> {len(approx)} [ {(distance_point):.2f}]",
                     (x, y),
                     cv2.FONT_HERSHEY_SIMPLEX,
                     1,
-                    (255, 0, 0),
+                    (255, 0, 255),
                     2,
                 )
                 cv2.circle(frame, center_obj, 5, (0, 0, 0), -1)
                 cv2.line(frame, center_coordinates, center_obj, (255, 0, 0), 2)
 
-    # print(near_point, abs_near_point)
+    print(near_point, abs_near_point)
     cv2.circle(frame, center_coordinates, 5, (0, 0, 255), -1)
     # Display the resulting frame
     cv2.imshow("frame", frame)
