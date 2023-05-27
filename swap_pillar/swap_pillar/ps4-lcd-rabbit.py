ecb8b186a2b7eb87c7cac939071c797916c32468
_Entity014 <phytes.narawit@gmail.com>
2023-05-27 07:00:15 +0700

    final past 4

--- swap_pillar/swap_pillar/ps4-lcd-rabbit.py
+++ swap_pillar/swap_pillar/ps4-lcd-rabbit.py
@@ -150,21 +150,30 @@ class Ps4(Node):
                 y = -1 * (self.axes["LX"])
                 x = -1 * self.axes["LY"]
                 if self.stateDriveMode == 1:
-                    if (x > limit) and (y > limit):
-                        x = 0.707
-                        y = 0.707
-                    elif (x < in_limit) and (y > limit):
-                        x = -0.707
-                        y = 0.707
-                    elif (x < in_limit) and (y < in_limit):
-                        x = -0.707
-                        y = -0.707
-                    elif (x > limit) and (y < in_limit):
-                        x = 0.707
-                        y = -0.707
+                    if x != 0.0 and y == 0.0:
+                        x = (abs(x) / x) * 0.55
+                    elif x == 0.0 and y != 0.0:
+                        y = (abs(y) / y) * 0.55
                     else:
                         x = 0.0
                         y = 0.0
+                    # if y != 0.0:
+                    #     y = (abs(y) / y) * 0.55
+                    # if (x > limit) and (y > limit):
+                    #     x = 0.707
+                    #     y = 0.707
+                    # elif (x < in_limit) and (y > limit):
+                    #     x = -0.707
+                    #     y = 0.707
+                    # elif (x < in_limit) and (y < in_limit):
+                    #     x = -0.707
+                    #     y = -0.707
+                    # elif (x > limit) and (y < in_limit):
+                    #     x = 0.707
+                    #     y = -0.707
+                    # else:
+                    #     x = 0.0
+                    #     y = 0.0
                 elif self.stateDriveMode == 2:
                     self.stateDriveMode = 0
 
